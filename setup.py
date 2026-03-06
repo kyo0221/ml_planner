a27073a98c6bbf5a2134eae2b8ca2c24b96f8b2b
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-03-06 10:59:17 +0900

    ✨feat: add entry

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'ml_planner'
 
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +27,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'planner_node = ml_planner.planner:main',
         ],
     },
 )
