34b40368f286a536fa77c47a27bd2558093be0d6
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-03-10 23:31:57 +0900

    ✨feat: 指令値推論

--- scripts/network.py
+++ scripts/network.py
@@ -1,42 +1,24 @@
 import torch
 import torch.nn as nn
+import timm
 
 
 class Network(nn.Module):
-    def __init__(self, num_waypoints: int = 10):
-        super(Network, self).__init__()
-        self.num_waypoints = num_waypoints
+    def __init__(self):
+        super().__init__()
 
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 24, kernel_size=5, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(24, 36, kernel_size=5, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(36, 48, kernel_size=5, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(48, 64, kernel_size=3, stride=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1),
-            nn.ReLU(),
-        )
+        self.backbone = timm.create_model("efficientnetv2_s", pretrained=True, num_classes=0)
 
         self.mlp = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(64 * 12 * 23, 256),
+            nn.Linear(self.backbone.num_features, 256),
             nn.ReLU(),
             nn.Dropout(p=0.3),
             nn.Linear(256, 128),
             nn.ReLU(),
-            nn.Linear(128, self.num_waypoints * 2),
+            nn.Linear(128, 1),
         )
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.backbone(x)
         x = self.mlp(x)
-        return x.view(x.shape[0], self.num_waypoints, 2)
-
-
-class ADELoss(nn.Module):
-    def forward(self, pred, target):
-        l2 = torch.linalg.norm(pred - target, dim=-1)
-        return l2.mean()
+        return x
