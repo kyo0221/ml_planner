b27ceccb1f0b2de1d3ae482e3749dc42c67a8429
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-03-12 12:37:23 +0900

    ✨feat: add command data

--- scripts/create_data.py
+++ scripts/create_data.py
@@ -8,7 +8,7 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_system_default
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Empty
+from std_msgs.msg import Empty, UInt8
 
 import cv2
 
@@ -27,9 +27,9 @@ class DataCreator(Node):
 
         self.create_subscription(Empty, '/flag', self.flag_callback, qos_profile_system_default)
         self.create_subscription(Twist, '/cmd_vel', self.vel_callback, qos_profile_system_default)
+        self.create_subscription(UInt8, '/command', self.command_callback, qos_profile_system_default)
         self.create_timer(SAMPLE_INTERVAL, self.timer_callback)
         
-
     def flag_callback(self, _msg):
         self.collect_flag = not self.collect_flag
         if self.collect_flag:
@@ -40,12 +40,15 @@ class DataCreator(Node):
     def vel_callback(self, msg):
         self.latest_vel = msg
 
+    def command_callback(self, msg):
+        self.command = msg.data
+
     def timer_callback(self):
         if not self.zed.grab() or not self.collect_flag:
             return
 
         image = self.zed.get_image()
-        self.collected_data.append((image, self.latest_vel))
+        self.collected_data.append((image, self.latest_vel, self.command))
         self.get_logger().info(f'🟢Collected data #{len(self.collected_data)}')
 
     def save_data(self) -> None:
@@ -60,19 +63,26 @@ class DataCreator(Node):
         
         images_dir = dataset_dir / 'images'
         action_dir = dataset_dir / 'actions'
+        command_dir = dataset_dir / 'commands'
 
         images_dir.mkdir(parents=True, exist_ok=True)
         action_dir.mkdir(parents=True, exist_ok=True)
+        command_dir.mkdir(parents=True, exist_ok=True)
 
-        for idx, (image, action) in enumerate(self.collected_data, start=1):
+        for idx, (image, action, command) in enumerate(self.collected_data, start=1):
             image_path = images_dir / f'{idx:05d}.png'
-            csv_path = action_dir / f'{idx:05d}.csv'
+            action_path = action_dir / f'{idx:05d}.csv'
+            command_path = command_dir / f'{idx:05d}.csv'
 
             cv2.imwrite(str(image_path), image)
 
-            with open(str(csv_path), 'w', newline='') as csvfile:
+            with open(str(action_path), 'w', newline='') as csvfile:
                 csv_writer = csv.writer(csvfile)
                 csv_writer.writerow([action.linear.x, action.angular.z])
+
+            with open(str(command_path), 'w', newline='') as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow([command])
 
         self.get_logger().info(f'🔵Saved {len(self.collected_data)} samples to {dataset_dir}')
 
